f1120d75d0f167584ca6c4787d67bb592cbb54e1
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2024-07-03 00:15:58 +0200

    moved the training in GPU space

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import flwr as fl
 from flwr.server.strategy import FedAvg
 from flwr.simulation import start_simulation
-from model import CnnEmnist
-from training import train, evaluate
-from parameters import device, writers_to_include, num_rounds
+from parameters import writers_to_include, num_rounds
 from data import get_dataloaders
-from client import FlowerClient, get_client_generator
+from client import get_client_generator
 
 
 train_loaders, val_loaders = get_dataloaders(num_writers=writers_to_include)
@@ -24,5 +22,5 @@ history = start_simulation(
     num_clients=writers_to_include,
     config=fl.server.ServerConfig(num_rounds=num_rounds),
     strategy=strategy,
-    client_resources={"num_cpus": 2, "num_gpus": 0.0},
+    client_resources={"num_cpus": 2, "num_gpus": 0.167},
 )
--- parameters.py
+++ parameters.py
@@ -5,11 +5,12 @@ import torch.nn as nn
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 writers_to_include = 100
 num_rounds = 5
+hybrid_ratio = 0.2
 
 
 # hyper parameters
 learning_rate = 0.01
 momentum = 0.9
 num_epochs = 10
-batch_size = 64
+batch_size = 32
 criterion = nn.CrossEntropyLoss()
